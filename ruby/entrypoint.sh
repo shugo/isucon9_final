5a5c5d442353cecd53805fc2fcca7bdbc360168e
Shugo Maeda <shugo@ruby-lang.org>
2019-10-05 14:16:08 +0900

    pumaにした。productionにした

--- ruby/entrypoint.sh
+++ ruby/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bundle install --path vendor/bundle
-bundle exec falcon -b tcp://0.0.0.0:8000 --hybrid --forks 2 --threads 4
-
+# bundle exec falcon -b tcp://0.0.0.0:8000 --hybrid --forks 2 --threads 4
+bundle exec puma -b tcp://0.0.0.0:8000 -e production -w 4
