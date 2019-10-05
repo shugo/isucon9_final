e4f0fd1359c6ccbaefdefd7ef29f61d53c18a3ac
Shugo Maeda <shugo@ruby-lang.org>
2019-10-05 17:47:38 +0900

    8に修正

--- ruby/entrypoint.sh
+++ ruby/entrypoint.sh
@@ -2,4 +2,4 @@
 
 bundle install --path vendor/bundle
 # bundle exec falcon -b tcp://0.0.0.0:8000 --hybrid --forks 2 --threads 4
-bundle exec puma -b tcp://0.0.0.0:8000 -e production -w 4
+bundle exec puma -b tcp://0.0.0.0:8000 -e production -w 8
